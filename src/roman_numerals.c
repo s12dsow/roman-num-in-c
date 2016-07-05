7300ce9a31c87795b56141484926affa994f585f
Dicko Sow <s12.dsow@gmail.com>
2016-07-05 10:22:52 -0400

    refactor convert to arabic method to use struct

--- src/roman_numerals.c
+++ src/roman_numerals.c
@@ -8,72 +8,47 @@ void add(char *first_val, char *second_val, char *roman_num) {
     convert_to_roman_num(sum, roman_num);
 }
 
-int convert_to_arabic_num(char *val) {
+typedef struct {
+	char *roman;
+	int num;
+} roman_converter;
+
+static const roman_converter ROMAN_TO_ARABIC_CONVERSION[] = {
+	{ .roman = "M", .num = 1000 },
+	{ .roman = "CM", .num = 900 },
+	{ .roman = "D", .num = 500 },
+	{ .roman = "CD", .num = 400 },
+	{ .roman = "C", .num = 100 },
+	{ .roman = "XC", .num = 90 },
+	{ .roman = "L", .num = 50 },
+	{ .roman = "XL", .num = 40 },
+	{ .roman = "X", .num = 10 },
+	{ .roman = "IX", .num = 9 },
+	{ .roman = "V", .num = 5 },
+	{ .roman = "IV", .num = 4 },
+	{ .roman = "I", .num = 1 }
+};
+
+
+int convert_to_arabic_num(char *roman_numeral) {
 	int arabic_num = 0;
-	int len = strlen(val);
+	int len = strlen(roman_numeral);
+	int roman_converter_len = sizeof(ROMAN_TO_ARABIC_CONVERSION) / sizeof(roman_converter);
 
 	for (int i = 0; i < len; i++) {
+		for(int j = 0; j < roman_converter_len; j++) {
 
-		if(strncmp(val + i, "M", 1) == 0) {
-			arabic_num += 1000;
+			const roman_converter converter = ROMAN_TO_ARABIC_CONVERSION[j];
+			int roman_len = strlen(converter.roman);
+
+			if(strncmp(roman_numeral + i, converter.roman, roman_len) == 0) {
+
+				arabic_num += converter.num;
+				i += roman_len - 1;
+			}
 		}
 
-		if(strncmp(val + i, "CM", 2) == 0) {
-			arabic_num += 900;
-			i += 1;
-		}
-
-		else if(strncmp(val + i, "D", 1) == 0) {
-			arabic_num += 500;
-		}
-
-		else if(strncmp(val + i, "CD", 2) == 0) {
-			arabic_num += 400;
-			i += 1;
-		}
-
-		else if(strncmp(val + i, "C", 1) == 0) {
-			arabic_num += 100;
-		}
-
-		else if(strncmp(val + i, "XC", 2) == 0) {
-			arabic_num += 90;
-			i += 1;
-		}
-
-		else if(strncmp(val + i, "L", 1) == 0) {
-			arabic_num += 50;
-		}
-
-		else if(strncmp(val + i, "XL", 2) == 0) {
-			arabic_num += 40;
-			i += 1;
-		}
-
-		else if(strncmp(val + i, "X", 1) == 0) {
-			arabic_num += 10;
-		}
-
-		else if(strncmp(val + i, "IX", 2) == 0) {
-			arabic_num += 9;
-			i += 1;
-		}
-		else if(strncmp(val + i, "V", 1) == 0) {
-
-			arabic_num += 5;
-		}
-
-		else if(strncmp(val + i, "IV", 2) == 0) {
-
-			arabic_num += 4;
-			i += 1;
-		}
-
-		else if(strncmp(val + i, "I", 1) == 0) {
-
-			arabic_num += 1;
-		}
-	} 
+	}
 	return arabic_num;
 }
 
