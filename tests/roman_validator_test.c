66715b0356f8598278ca080e7f5c076e540135fd
Dicko Sow <s12.dsow@gmail.com>
2016-08-01 00:19:50 -0400

    renamed tests for validator

--- tests/roman_validator_test.c
+++ tests/roman_validator_test.c
@@ -1,28 +1,28 @@
 #include <check.h>
 #include "../src/roman_validator.h"
 
-START_TEST (test_convert_A_is_invalid)
+START_TEST (test_input_A_is_invalid)
 {
     int num = is_valid_numeral("A");
     ck_assert_int_eq(num, 0);
 }
 END_TEST
 
-START_TEST (test_convert_IIII_is_invalid)
+START_TEST (test_input_IIII_is_invalid)
 {
     int num = is_valid_numeral("IIII");
     ck_assert_int_eq(num, 0);
 }
 END_TEST
 
-START_TEST (test_convert_MMDDCC_is_invalid)
+START_TEST (test_input_MMDDCC_is_invalid)
 {
     int num = is_valid_numeral("MMDDCC");
     ck_assert_int_eq(num, 0);
 }
 END_TEST
 
-START_TEST (test_convert_IIZJID_is_invalid)
+START_TEST (test_input_IIZJID_is_invalid)
 {
     int num = is_valid_numeral("IIZJID");
     ck_assert_int_eq(num, 0);
@@ -38,10 +38,10 @@ Suite* roman_validator_suite(void)
 
 	tc_roman_validator = tcase_create("Roman Validator");
 
-	tcase_add_test(tc_roman_validator, test_convert_A_is_invalid);
-	tcase_add_test(tc_roman_validator, test_convert_IIII_is_invalid);
-	tcase_add_test(tc_roman_validator, test_convert_MMDDCC_is_invalid);
-	tcase_add_test(tc_roman_validator, test_convert_IIZJID_is_invalid);
+	tcase_add_test(tc_roman_validator, test_input_A_is_invalid);
+	tcase_add_test(tc_roman_validator, test_input_IIII_is_invalid);
+	tcase_add_test(tc_roman_validator, test_input_MMDDCC_is_invalid);
+	tcase_add_test(tc_roman_validator, test_input_IIZJID_is_invalid);
 
 	suite_add_tcase(s, tc_roman_validator);
 
